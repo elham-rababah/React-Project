a0cebd70b966b0d0ec446a3e282f7b2973e306d1
Elham <elham.a.rababah@gmail.com>
2018-01-17 21:06:49 +0200

    first step from productscomponent

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,17 @@
 //
 import React from 'react';
+import Productscomponent from './productscomponent.jsx';
+
 import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React project</p>;
+    return (
+    	<div>
+	    	<p> Hello React project</p>
+	    	<Productscomponant/>
+    	</div>
+    	);
   }
 }
 
---
+++ app/productscomponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Productscomponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+ 
+
+  render() {
+    return (
+      <div>
+        Test the Productscomponent
+      </div>
+    );
+  }
+
+}
+
+export default Productscomponent;
