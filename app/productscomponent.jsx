b42a114ebc17a346b603ff5d9c8b36629d347c73
Elham <elham.a.rababah@gmail.com>
2018-01-18 13:31:26 +0200

    first step for preview data

--- app/productscomponent.jsx
+++ app/productscomponent.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactTable from 'react-table'
+
 
 class Productscomponent extends React.Component {
 
@@ -8,13 +10,54 @@ class Productscomponent extends React.Component {
 
  
 
-  render() {
+render() {
+  
+  const data = [{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+      name: 'Jason Maurer',
+      age: 23,
+    }
+  },
+  {
+    name: 'eee Linsley',
+    age: 86,
+    friend: {
+      name: 'rrr Maurer',
+      age: 93,
+    }
+  }
+
+  ]
+
+  const columns = [{
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+  }, {
+    Header: 'Age',
+    accessor: 'age',
+    Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+  }, {
+    id: 'friendName', // Required because our accessor is not a string
+    Header: 'Friend Name',
+    accessor: d => d.friend.name // Custom value accessors!
+  }, {
+    Header: props => <span>Friend Age</span>, // Custom header components!
+    accessor: 'friend.age'
+  }];
+  return(
+  <ReactTable
+    data={data} columns={columns}/>);
+  }
+
+  /*render() {
     return (
       <div>
         Test the Productscomponent
       </div>
     );
-  }
+  }*/
 
 }
 
