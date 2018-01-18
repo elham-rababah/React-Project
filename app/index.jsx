a9426d3db86c811bbe89a034ffc5b8bc31475ade
Elham <elham.a.rababah@gmail.com>
2018-01-18 11:59:01 +0200

    fix small bug

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 //
 import React from 'react';
-import Productscomponent from './productscomponent.jsx';
-
 import {render} from 'react-dom';
+
+import  Productscomponent  from './productscomponent.jsx';
+
 
 class App extends React.Component {
   render () {
     return (
     	<div>
 	    	<p> Hello React project</p>
-	    	<Productscomponant/>
+	    	<Productscomponent/> 
     	</div>
     	);
   }
